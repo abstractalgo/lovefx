fb6a6b3c9c07119dd824c300b414d7fd3fd09d8b
Nordeus3 <largo.etf@gmail.com>
2016-03-07 18:59:55 +0100

    res modd

--- effects/008_res.cpp
+++ effects/008_res.cpp
@@ -54,34 +54,38 @@ void h_keyUp(unsigned int k)
 
 void resize_mipmap()
 {
-
+    PerfMarker("mipmap", 0xFFFFC90E);
 }
 void resize_shader()
 {
-    
+    PerfMarker("shader", 0xFF880015);
+    glDisable(GL_DEPTH_TEST);
+    lovefx::fbo::use(smallRT.fbo);
+    lovefx::program::use(resshdr.program);
+    glBindTexture(GL_TEXTURE_2D, defRT.col0);
+    glUniform1i(resshdr.inTex_uloc, 0);
+    lovefx::utils::drawFSQuad(fsquad);
+    glEnable(GL_DEPTH_TEST);
 }
 void resize_compute()
 {
-
+    PerfMarker("compute", 0xFF3F48CC);
 }
 
 void resize()
 {
     if (1 == resizeMode)
     {
-        PerfMarker("mipmap", 0xFFFFC90E);
         resize_mipmap();
     }
 
     else if (2 == resizeMode)
     {
-        PerfMarker("shader", 0xFF880015);
         resize_shader();
     }
     
     else //if (3 == resizeMode)
     {
-        PerfMarker("compute", 0xFF3F48CC);
         resize_compute();
     }
 }
@@ -90,6 +94,7 @@ void resize()
 
 void InitApp()
 {
+    PerfMarker("INIT", 0xFF0000FF);
     // globals
     {
         PerfMarker("g_init", 0xFF8888FF);
@@ -100,6 +105,8 @@ void InitApp()
         lovefx::utils::initFSQuad(fsquad);
         // default render target
         lovefx::rt::create(defRT, width, height);
+        // small RT
+        lovefx::rt::create(smallRT, width/2, height/2);
         // init shaders
         const char* vs = LFX_GLSL(330,
             layout(location = 0) in vec2 aPosition;
@@ -151,6 +158,10 @@ void InitApp()
             void main()
             {
                 vec4 col = texture(inTex, uv);
+                for (int i = 0; i < 20; i++)
+                {
+                    col = texture(inTex, uv);
+                }
                 fragColor = col;
             }
         );
@@ -163,24 +174,30 @@ void InitApp()
 
 void RenderApp()
 {
-    PerfMarker("scene", 0xFFFF6666);
+    PerfMarker("frame", 0xFFFF6666);
     // 1. render a scene
-    lovefx::fbo::use(defRT.fbo);
-    glClearColor(1, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // TODO: render scene
+    {
+        PerfMarker("scene", 0xFF494949);
+        lovefx::fbo::use(defRT.fbo);
+        glClearColor(1, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // TODO: render scene
+    }
 
     // 2. do a resize
     resize();
 
     // 3. display
-    lovefx::fbo::use(GL_NONE);
-    glClearColor(0, 0, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    lovefx::program::use(displayProgram);
-    glBindTexture(GL_TEXTURE_2D, defRT.col0);
-    glUniform1i(inTex_uloc, 0);
-    lovefx::utils::drawFSQuad(fsquad);
+    {
+        PerfMarker("display", 0xFF888888);
+        lovefx::fbo::use(GL_NONE);
+        glClearColor(0, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        lovefx::program::use(displayProgram);
+        glBindTexture(GL_TEXTURE_2D, smallRT.col0);
+        glUniform1i(inTex_uloc, 0);
+        lovefx::utils::drawFSQuad(fsquad);
+    }
 
     LFX_ERRCHK();
 }
